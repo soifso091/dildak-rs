3269f74648d76abb13de0b20da67ed2b6357ec00
soifso091 <soifso091@gmail.com>
2025-10-07 11:49:00 +0300

    new cheat fixed

--- core/src/main/java/io/github/necrashter/natural_revenge/world/player/Player.java
+++ core/src/main/java/io/github/necrashter/natural_revenge/world/player/Player.java
@@ -398,6 +398,6 @@ public class Player extends GameEntity {
     @Override
     public boolean takeDamage(float amount, DamageAgent agent, DamageSource source) {
         world.screen.playerHurt();
-        return;
+        return 0;
     }
 }
